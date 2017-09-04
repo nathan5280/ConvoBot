3884d217b7d748dfa3c820a284bee241b6e26826
Nathan Atkins <atkinsnw@gmail.com>
2017-09-04 12:42:54 -0600

    Cleanup deleted files.

--- Simulator/scripts/nameserver.bash
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python -m Pyro4.naming
--- Simulator/scripts/snake-shake-foreground.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Copy the Env class to a directory where Blender can find it.
-cp src/Env.py /Applications/blender.app/Contents/Resources/2.78/python/lib/python3.5/site-packages/snakeshake
-cp src/RadialEnv.py /Applications/blender.app/Contents/Resources/2.78/python/lib/python3.5/site-packages/snakeshake
-
-# Start blender with the UI and load the SnakeShake foreground server.s
-/Applications/blender.app/Contents/MacOS/blender -P src/SnakeShakeFGServer.py
