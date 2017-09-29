efaa00a26888937725aa92acc251ef4965e6f183
Nathan Atkins <atkinsnw@gmail.com>
2017-09-29 12:51:36 -0600

    Added Convobot CNN Model

--- scripts/cv-32x32.bash
+++ scripts/cv-32x32.bash
@@ -7,8 +7,8 @@
 # Create the images in blender
 # python simulate_images.py -d convobot -e config -c test
 
-# Convert the images to 28x28 grayscale
-python prepare_images.py -d convobot -e config -c test-gs
+# Convert the images to 32x32 grayscale
+# python prepare_images.py -d convobot -e config -c color-32x32
 
 # Run the model 1-run, 1-epoch on the grayscale images.
-# python run_model.py -d convobot -e config -m mnist-gs -c test-gs
+python run_model.py -d convobot -e config -m convobot -c cv-32x32
---
+++ scripts/predict-color-32x32.bash
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python predict_validation.py -d convobot -e config -m mnist-color -c color-32x32
